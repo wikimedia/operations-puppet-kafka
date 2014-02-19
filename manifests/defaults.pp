e907aa2865f7d57b66d4a273b888415086ad2048
Andrew Otto <acotto@gmail.com>
2014-02-19 14:55:01 -0500

    Parameterizing 4 more replica server settings
    Change-Id: I07e0c127f948aada1c7439e1bb379e1811cbd1fe

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -15,61 +15,67 @@ class kafka::defaults {
         },
     }
 
-    $zookeeper_hosts                 = ['localhost:2181']
-    $zookeeper_connection_timeout_ms = 1000000
-    $zookeeper_chroot                = undef
+    $zookeeper_hosts                     = ['localhost:2181']
+    $zookeeper_connection_timeout_ms     = 1000000
+    $zookeeper_chroot                    = undef
 
-    $kafka_log_file                  = '/var/log/kafka/kafka.log'
-    $producer_type                   = 'async'
-    $producer_batch_num_messages     = 200
-    $consumer_group_id               = 'test-consumer-group'
+    $kafka_log_file                      = '/var/log/kafka/kafka.log'
+    $producer_type                       = 'async'
+    $producer_batch_num_messages         = 200
+    $consumer_group_id                   = 'test-consumer-group'
 
     # Broker Server settings
-    $jmx_port                        = 9999
-    $log_dirs                        = ['/var/spool/kafka']
-    $heap_opts                       = undef
-    $nofiles_ulimit                  = 8192
+    $jmx_port                            = 9999
+    $log_dirs                            = ['/var/spool/kafka']
+    $heap_opts                           = undef
+    $nofiles_ulimit                      = 8192
 
-    $auto_create_topics_enable       = false
-    $num_replica_fetchers            = 1
-    $replica_fetch_max_bytes         = 1048576
+    $auto_create_topics_enable           = false
 
-    $num_network_threads             = 2
-    $num_io_threads                  = 2
-    $socket_send_buffer_bytes        = 1048576
-    $socket_receive_buffer_bytes     = 1048576
-    $socket_request_max_bytes        = 104857600
+    $replica_lag_time_max_ms             = undef
+    $replica_lag_max_messages            = undef
+    $replica_socket_timeout_ms           = undef
+    $replica_socket_receive_buffer_bytes = undef
+    $num_replica_fetchers                = 1
+    $replica_fetch_max_bytes             = 1048576
 
-    $log_flush_interval_messages     = 10000
-    $log_flush_interval_ms           = 1000
+    $num_network_threads                 = 2
+    $num_io_threads                      = 2
+    $socket_send_buffer_bytes            = 1048576
+    $socket_receive_buffer_bytes         = 1048576
+    $socket_request_max_bytes            = 104857600
 
-    $log_retention_hours             = 168     # 1 week
-    $log_retention_bytes             = undef
-    $log_segment_bytes               = 536870912
+    $log_flush_interval_messages         = 10000
+    $log_flush_interval_ms               = 1000
 
-    $log_cleanup_policy              = 'delete'
-    $log_cleanup_interval_mins       = 1
+    $log_retention_hours                 = 168     # 1 week
+    $log_retention_bytes                 = undef
+    $log_segment_bytes                   = 536870912
 
-    $metrics_properties              = undef
+    $log_cleanup_policy                  = 'delete'
+    $log_cleanup_interval_mins           = 1
+
+    $metrics_properties                  = undef
 
     # Kafka package version.
-    $version                         = 'installed'
+    $version                             = 'installed'
 
     # MirrorMaker settings
-    $topic_whitelist                 = '.*'
-    $topic_blacklist                 = undef
-    $num_streams                     = 1
-    $num_producers                   = 1
-    $queue_size                      = 10000
+    $topic_whitelist                     = '.*'
+    $topic_blacklist                     = undef
+    $num_streams                         = 1
+    $num_producers                       = 1
+    $queue_size                          = 10000
 
     # Default puppet paths to template config files.
     # This allows us to use custom template config files
     # if we want to override more settings than this
     # module yet supports.
-    $producer_properties_template    = 'kafka/producer.properties.erb'
-    $consumer_properties_template    = 'kafka/consumer.properties.erb'
-    $log4j_properties_template       = 'kafka/log4j.properties.erb'
-    $server_properties_template      = 'kafka/server.properties.erb'
-    $server_default_template         = 'kafka/kafka.default.erb'
-    $mirror_default_template         = 'kafka/kafka-mirror.default.erb'
+    $producer_properties_template        = 'kafka/producer.properties.erb'
+    $consumer_properties_template        = 'kafka/consumer.properties.erb'
+    $log4j_properties_template           = 'kafka/log4j.properties.erb'
+    $server_properties_template          = 'kafka/server.properties.erb'
+    $server_default_template             = 'kafka/kafka.default.erb'
+    $mirror_default_template             = 'kafka/kafka-mirror.default.erb'
+
 }
--- manifests/server.pp
+++ manifests/server.pp
@@ -3,133 +3,151 @@
 #
 # == Parameters:
 
-# $enabled                          - If false, Kafka Broker Server will not be
-#                                     started.  Default: true.
+# $enabled                              - If false, Kafka Broker Server will not be
+#                                         started.  Default: true.
 #
-# $brokers                          - Hash of Kafka Broker configs keyed by
-#                                     fqdn of each kafka broker node.  This Hash
-#                                     should be of the form:
-#                                       { 'hostA' => { 'id' => 1, 'port' => 12345 }, 'hostB' => { 'id' => 2 }, ... }
-#                                     'port' is optional, and will default to 9092.
+# $brokers                              - Hash of Kafka Broker configs keyed by
+#                                         fqdn of each kafka broker node.  This Hash
+#                                         should be of the form:
+#                                           { 'hostA' => { 'id' => 1, 'port' => 12345 }, 'hostB' => { 'id' => 2 }, ... }
+#                                         'port' is optional, and will default to 9092.
 #
-# $log_dirs                         - Array of directories in which the broker will store its
-#                                     received log event data.
-#                                     (This is log.dir in server.properties).
-#                                     Default: [/var/spool/kafka]
+# $log_dirs                             - Array of directories in which the broker will store its
+#                                         received log event data.
+#                                         (This is log.dir in server.properties).
+#                                         Default: [/var/spool/kafka]
 #
-# $zookeeper_hosts                  - Array of zookeeper hostname/IP(:port)s.
-#                                     Default: ['localhost:2181]
+# $zookeeper_hosts                      - Array of zookeeper hostname/IP(:port)s.
+#                                         Default: ['localhost:2181]
 #
-# $zookeeper_connection_timeout_ms  - Timeout in ms for connecting to zookeeper.
-#                                     Default: 1000000
+# $zookeeper_connection_timeout_ms      - Timeout in ms for connecting to zookeeper.
+#                                         Default: 1000000
 #
-# $zookeeper_chroot                 - Path in zookeeper in which to keep Kafka data.
-#                                     Default: undef (the root znode).  Note that if you set
-#                                     this paramater, the znode will not be created for you.
-#                                     You must do so manually yourself.  See the README
-#                                     for instructions on how to do so.
+# $zookeeper_chroot                     - Path in zookeeper in which to keep Kafka data.
+#                                         Default: undef (the root znode).  Note that if you set
+#                                         this paramater, the znode will not be created for you.
+#                                         You must do so manually yourself.  See the README
+#                                         for instructions on how to do so.
 #
-# $jmx_port                         - Port on which to expose JMX metrics.  Default: 9999
+# $jmx_port                             - Port on which to expose JMX metrics.  Default: 9999
 #
-# $heap_opts                        - Heap options to pass to JVM on startup.  Default: undef
+# $heap_opts                            - Heap options to pass to JVM on startup.  Default: undef
 #
-# $nofiles_ulimit                   - If set, the kafka user's ulimit will be set to this, and the
-#                                     kafka server will set this via ulimit -n.  You
-#                                     will probably need to reboot for this to take effect.
+# $nofiles_ulimit                       - If set, the kafka user's ulimit will be set to this, and the
+#                                         kafka server will set this via ulimit -n.  You
+#                                         will probably need to reboot for this to take effect.
 #
-# $auto_create_topics_enable        - If autocreation of topics is allowed.  Default: false
+# $auto_create_topics_enable            - If autocreation of topics is allowed.  Default: false
 #
-# $num_replica_fetchers             - Number of threads used to replicate messages from leaders.
-#                                     Default: 1
+# $replica_lag_time_max_ms              - If a follower hasn't sent any fetch requests for this window
+#                                         of time, the leader will remove the follower from ISR.
+#                                         Default: undef
+# $replica_lag_max_messages             - If a replica falls more than this many messages behind the leader,
+#                                         the leader will remove the follower from ISR.
+#                                         Default: undef
 #
-# $replica_fetch_max_bytes          - The number of bytes of messages to attempt to fetch for each
-#                                     partition in the fetch requests the replicas send to the leader.
-#                                     Default: 1024 * 1024
+# $replica_socket_timeout_ms            - The socket timeout for network requests to the leader for
+#                                         replicating data.  Default: undef
 #
-# $num_network_threads              - The number of threads handling network
-#                                     requests.  Default: 2
+# $replica_socket_receive_buffer_bytes  - The socket receive buffer for network requests to the leader
+#                                         for replicating data.  Default: undef
 #
-# $num_io_threads                   - The number of threads doing disk I/O.  Default: 2
+# $num_replica_fetchers                 - Number of threads used to replicate messages from leaders.
+#                                         Default: 1
 #
-# $socket_send_buffer_bytes         - The byte size of the send buffer (SO_SNDBUF)
-#                                      used by the socket server.  Default: 1048576
+# $replica_fetch_max_bytes              - The number of bytes of messages to attempt to fetch for each
+#                                         partition in the fetch requests the replicas send to the leader.
+#                                         Default: 1024 * 1024
 #
-# $socket_receive_buffer_bytes      - The byte size of receive buffer (SO_RCVBUF)
-#                                     used by the socket server.  Default: 1048576
+# $num_network_threads                  - The number of threads handling network
+#                                         requests.  Default: 2
 #
-# $socket_request_max_bytes         - The maximum size of a request that the socket
-#                                     server will accept.  Default: 104857600
+# $num_io_threads                       - The number of threads doing disk I/O.  Default: 2
 #
-# $log_flush_interval_messages      - The number of messages to accept before
-#                                     forcing a flush of data to disk.  Default 10000
+# $socket_send_buffer_bytes             - The byte size of the send buffer (SO_SNDBUF)
+#                                          used by the socket server.  Default: 1048576
 #
-# $log_flush_interval_ms            - The maximum amount of time a message can sit
-#                                     in a log before we force a flush: Default 1000 (1 second)
+# $socket_receive_buffer_bytes          - The byte size of receive buffer (SO_RCVBUF)
+#                                         used by the socket server.  Default: 1048576
 #
-# $log_retention_hours              - The minimum age of a log file to be eligible
-#                                     for deletion.  Default 1 week
+# $socket_request_max_bytes             - The maximum size of a request that the socket
+#                                         server will accept.  Default: 104857600
 #
-# $log_retention_size               - A size-based retention policy for logs.
-#                                     Default: undef (disabled)
+# $log_flush_interval_messages          - The number of messages to accept before
+#                                         forcing a flush of data to disk.  Default 10000
 #
-# $log_segment_bytes                - The maximum size of a log segment file. When
-#                                     this size is reached a new log segment will
-#                                     be created:  Default 536870912 (512MB)
+# $log_flush_interval_ms                - The maximum amount of time a message can sit
+#                                         in a log before we force a flush: Default 1000 (1 second)
 #
-# $log_cleanup_interval_mins        - The interval at which log segments are checked
-#                                     to see if they can be deleted according to the
-#                                     retention policies.  Default: 1
+# $log_retention_hours                  - The minimum age of a log file to be eligible
+#                                         for deletion.  Default 1 week
 #
-# $log_cleanup_policy               - The default policy for handling log tails.
-#                                     Can be either delete or dedupe.  Default: delete
+# $log_retention_size                   - A size-based retention policy for logs.
+#                                         Default: undef (disabled)
 #
-# $metrics_properties               - Config hash of Kafka metrics property key => value pairs.
-#                                     Use this for configuring your own metrics reporter classes.
-#                                     Default: undef
+# $log_segment_bytes                    - The maximum size of a log segment file. When
+#                                         this size is reached a new log segment will
+#                                         be created:  Default 536870912 (512MB)
 #
-# $kafka_log_file                   - File in which to write Kafka logs (not event message data).
-#                                     Default: /var/log/kafka/kafka.log
+# $log_cleanup_interval_mins            - The interval at which log segments are checked
+#                                         to see if they can be deleted according to the
+#                                         retention policies.  Default: 1
+#
+# $log_cleanup_policy                   - The default policy for handling log tails.
+#                                         Can be either delete or dedupe.  Default: delete
+#
+# $metrics_properties                   - Config hash of Kafka metrics property key => value pairs.
+#                                         Use this for configuring your own metrics reporter classes.
+#                                         Default: undef
+#
+# $kafka_log_file                       - File in which to write Kafka logs (not event message data).
+#                                         Default: /var/log/kafka/kafka.log
 
 #
 class kafka::server(
-    $enabled                         = true,
+    $enabled                             = true,
 
-    $brokers                         = $kafka::defaults::brokers,
-    $log_dirs                        = $kafka::defaults::log_dirs,
+    $brokers                             = $kafka::defaults::brokers,
+    $log_dirs                            = $kafka::defaults::log_dirs,
 
-    $zookeeper_hosts                 = $kafka::defaults::zookeeper_hosts,
-    $zookeeper_connection_timeout_ms = $kafka::defaults::zookeeper_connection_timeout_ms,
-    $zookeeper_chroot                = $kafka::defaults::zookeeper_chroot,
+    $zookeeper_hosts                     = $kafka::defaults::zookeeper_hosts,
+    $zookeeper_connection_timeout_ms     = $kafka::defaults::zookeeper_connection_timeout_ms,
+    $zookeeper_chroot                    = $kafka::defaults::zookeeper_chroot,
 
-    $jmx_port                        = $kafka::defaults::jmx_port,
-    $heap_opts                       = $kafka::defaults::heap_opts,
-    $nofiles_ulimit                  = $kafka::defaults::nofiles_ulimit,
+    $jmx_port                            = $kafka::defaults::jmx_port,
+    $heap_opts                           = $kafka::defaults::heap_opts,
+    $nofiles_ulimit                      = $kafka::defaults::nofiles_ulimit,
 
-    $auto_create_topics_enable       = $kafka::defaults::auto_create_topics_enable,
-    $num_replica_fetchers            = $kafka::defaults::num_replica_fetchers,
-    $replica_fetch_max_bytes         = $kafka::defaults::replica_fetch_max_bytes,
+    $auto_create_topics_enable           = $kafka::defaults::auto_create_topics_enable,
 
-    $num_network_threads             = $kafka::defaults::num_network_threads,
-    $num_io_threads                  = $kafka::defaults::num_io_threads,
-    $socket_send_buffer_bytes        = $kafka::defaults::socket_send_buffer_bytes,
-    $socket_receive_buffer_bytes     = $kafka::defaults::socket_receive_buffer_bytes,
-    $socket_request_max_bytes        = $kafka::defaults::socket_request_max_bytes,
+    $replica_lag_time_max_ms             = $kafka::defaults::replica_lag_time_max_ms,
+    $replica_lag_max_messages            = $kafka::defaults::replica_lag_max_messages,
+    $replica_socket_timeout_ms           = $kafka::defaults::replica_socket_timeout_ms,
+    $replica_socket_receive_buffer_bytes = $kafka::defaults::replica_socket_receive_buffer_bytes,
+    $num_replica_fetchers                = $kafka::defaults::num_replica_fetchers,
+    $replica_fetch_max_bytes             = $kafka::defaults::replica_fetch_max_bytes,
 
-    $log_flush_interval_messages     = $kafka::defaults::log_flush_interval_messages,
-    $log_flush_interval_ms           = $kafka::defaults::log_flush_interval_ms,
-    $log_retention_hours             = $kafka::defaults::log_retention_hours,
-    $log_retention_bytes             = $kafka::defaults::log_retention_bytes,
-    $log_segment_bytes               = $kafka::defaults::log_segment_bytes,
+    $num_network_threads                 = $kafka::defaults::num_network_threads,
+    $num_io_threads                      = $kafka::defaults::num_io_threads,
+    $socket_send_buffer_bytes            = $kafka::defaults::socket_send_buffer_bytes,
+    $socket_receive_buffer_bytes         = $kafka::defaults::socket_receive_buffer_bytes,
+    $socket_request_max_bytes            = $kafka::defaults::socket_request_max_bytes,
 
-    $log_cleanup_interval_mins       = $kafka::defaults::log_cleanup_interval_mins,
-    $log_cleanup_policy              = $kafka::defaults::log_cleanup_policy,
+    $log_flush_interval_messages         = $kafka::defaults::log_flush_interval_messages,
+    $log_flush_interval_ms               = $kafka::defaults::log_flush_interval_ms,
+    $log_retention_hours                 = $kafka::defaults::log_retention_hours,
+    $log_retention_bytes                 = $kafka::defaults::log_retention_bytes,
+    $log_segment_bytes                   = $kafka::defaults::log_segment_bytes,
 
-    $metrics_properties              = $kafka::defaults::metrics_properties,
-    $kafka_log_file                  = $kafka::defaults::kafka_log_file,
+    $log_cleanup_interval_mins           = $kafka::defaults::log_cleanup_interval_mins,
+    $log_cleanup_policy                  = $kafka::defaults::log_cleanup_policy,
 
-    $server_properties_template      = $kafka::defaults::server_properties_template,
-    $default_template                = $kafka::defaults::server_default_template,
-    $log4j_properties_template       = $kafka::defaults::log4j_properties_template
+    $metrics_properties                  = $kafka::defaults::metrics_properties,
+    $kafka_log_file                      = $kafka::defaults::kafka_log_file,
+
+    $server_properties_template          = $kafka::defaults::server_properties_template,
+    $default_template                    = $kafka::defaults::server_default_template,
+    $log4j_properties_template           = $kafka::defaults::log4j_properties_template
 ) inherits kafka::defaults
 {
     # Kafka class must be included before kafka::server.
