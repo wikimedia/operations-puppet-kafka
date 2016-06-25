1f621f6450fb86dd6bb7cf65eaa7ec28fda68ae6
Daniel Zahn <dzahn@wikimedia.org>
2016-06-25 03:00:10 -0700

    lint-ignore arrows in tests/server
    Change-Id: I2e1fd0490f105c15adbd37b022ea3cf057e732ff

--- tests/server.pp
+++ tests/server.pp
@@ -1,7 +1,9 @@
 class { 'kafka::server':
+    # lint:ignore:arrow_alignment
     brokers           => {
         "${::fqdn}"               => { 'id' => 1, 'port' => 12345 },
         'kafka-node02.domain.org' => { 'id' => 2 },
     },
+    # lint:endignore
     zookeeper_hosts => ['zk-node01:2181', 'zk-node02:2181', 'zk-node03:2181'],
 }
