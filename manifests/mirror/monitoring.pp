14408463adeae045442adeff7413dad6c05afc54
Antoine Musso <hashar@free.fr>
2017-02-17 17:21:57 +0100

    Introduce linters using rake
    Add a Rakefile and depends on puppet-syntax/puppet-lint. That lets one
    easily check syntax of manifests and templates as well as puppet style
    recommandation (puppet-lint).
    
    Ignore puppet-lint 80 chars line limit.
    Instruct puppet-lint to threat module with relative path or it fails
    under Jenkins which checkout the source code in a directory named after
    the job.
    
    Fix:
        Unrecognised escape sequence '\.' in file
        kafka/manifests/mirror/monitoring.pp at line 21
    
    Add Gem dependecy safe_yaml ~> 1.0.4 since Puppet 3.7 fails on ruby 2.2+
    (eg ruby2.4 on Mac). https://tickets.puppetlabs.com/browse/PUP-3796
    
    Git ignore Gemfile.lock, that is only needed for deployment purposes.
    
    To test locally:
    
      bundle install
      bundle exec rake test
    
    Bug: T154894
    Change-Id: Id8e19ee14cc1673e84278b30a5e4eddf5192ea77

--- manifests/mirror/monitoring.pp
+++ manifests/mirror/monitoring.pp
@@ -18,7 +18,7 @@ define kafka::mirror::monitoring(
     # Generate icinga alert if Kafka Server is not running.
     nrpe::monitor_service { "kafka-mirror-${title}":
         description  => "Kafka MirrorMaker ${title}",
-        nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -C java  --ereg-argument-array 'kafka.tools.MirrorMaker.+/etc/kafka/mirror/${title}/producer\.properties'",
+        nrpe_command => "/usr/lib/nagios/plugins/check_procs -c 1:1 -C java  --ereg-argument-array 'kafka.tools.MirrorMaker.+/etc/kafka/mirror/${title}/producer\\.properties'",
         require      => Kafka::Mirror[$title],
     }
 
